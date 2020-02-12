c4c78bea272cc7da3f980144206b8903b6a86d9c
anjagger <59902231+anjagger@users.noreply.github.com>
2020-02-11 21:58:27 -0500

    A moving space ship

--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -4,5 +4,14 @@ extends KinematicBody2D
 func _ready():
 	pass
 
-func _physics_process(delta):
-	pass
+func _physics_process(_delta):
+	if Input.is_action_pressed("left"):
+		position.x = position.x - 10
+	if Input.is_action_pressed("right"):
+		position.x = position.x + 10
+	if Input.is_action_pressed("down"):
+		position.y = position.y + 10
+	if Input.is_action_pressed("up"):
+		position.y = position.y - 10
+	var mouse_pos = get_viewport().get_mouse_position()
+	print(mouse_pos.x)
